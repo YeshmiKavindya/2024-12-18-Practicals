554a65bf209051d48a97c222712cbb90c9868182
Yeshmi Kavindya <139144140+YeshmiKavindya@users.noreply.github.com>
2024-12-22 16:12:30 +0000

    created files

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>2020ASP87 Practicals</h1><br><br>
+
+    <a href="getTable.php">Gettable</a><br>
+    <a href="printTable.php">printtable</a><br>
+
+    
+</body>
+</html>
